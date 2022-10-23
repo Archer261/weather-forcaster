ef23e8ed96515ee9ff2911f6a34f7c03f89f25ca
Joseph Archer <viewtiful.kja@gmail.com>
2022-10-23 17:00:14 -0400

    fix issue with local storage and created date format function for cards

--- script.js
+++ script.js
@@ -4,70 +4,93 @@ var APIKey = '0921cf8315a06211d5c903665ccab12f';
 // API Call Variables
 var city = 'Chicago';
 var queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKey}`
-var searchBtn = document.getElementById("search");
+var searchBtn = $("#search");
 
-// DOM Containers
-var cardRow = document.getElementById('forcast-cards');
-var cardCol = document.createElement('div').classList.add("col");
-var cardH = document.createElement('div').classList.add("card", "h-100");
-var cardBody = document.createElement('div').classList.add("card-body");
+// DOM Elements
+const cityHistory = $('#city-history');
 
-// searchBtn.addEventListener("click",)
 
-async function getForecastData() {
-    // Convert city name to lat and lon
-    await fetch(queryURL)
-        .then((res) => res.json())
-        .then((data) => console.log(data));
-    return data
+// Utilities
+function formatDate(date) {
+    return `${moment(date).format("M")}/${moment(date).format("DD")}/${moment(date).format("YYYY")}`
+}
+
+
+// Function that adds search history to local storage after api call and creates button
+function addCityHistory() {
+    searchBtn.on("click", () => {
+        newCity = $("#city-input").val();
+        if (newCity.length > 0) {
+            if (Object.keys(localStorage).includes(`ls-${newCity}`)) {
+                return;
+            } else {
+                localStorage.setItem(`ls-${newCity}`, newCity)
+                const add = $(`<div class=" m-1 row"><button id="ls-${newCity}" class="btn btn-secondary btn-block" type="submit">${newCity}</button></div>`);
+                cityHistory.append(add);
+            }
+        } else {
+            return;
+        }
+    })
 
 }
 
-// Convert city to latitude and longitude
-async function convertCity() {
-    let lat = '';
-    let lon = '';
-    await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=${APIKey}`)
-        .then((res) => res.json())
-        .then((data) => data.forEach((c) => {
-            if (c.country === 'US') {
-                console.log(c.lat, c.lon);
-                lat = c.lat;
-                lon = c.lon;
-            }
+async function getCityData() {
+    let obj;
+    const res = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=${APIKey}`)
+    obj = await res.json();
+    result = obj.filter(ct => ct.country === 'US')
+    return result;
+}
 
-        }))
-    return fetch(`http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${APIKey}`)
-        .then((response) => response.json())
-        .then((weekData) => {
-            let currentDate = moment().format("DD") - 1;
-            weekData.list.forEach((obj) => {
-                if (moment(obj.dt_txt).format("DD") > currentDate) {
-                    currentDate = moment(obj.dt_txt).format("DD");
-                    createWeekForcast(obj.dt_txt, obj.main.temp, obj.wind.speed, obj.main.humidity);
-                }
-            })
-        });
+async function convertCity() {
+    var cityData = await getCityData();
+    let obj;
+    let data = [];
+
+    const res = await fetch(`http://api.openweathermap.org/data/2.5/forecast?lat=${cityData[0].lat}&lon=${cityData[0].lon}&appid=${APIKey}`)
+
+    obj = await res.json();
+
+
+    let currentDate = moment().format("DD") - 1;
+    obj.list.forEach((o) => {
+        if (moment(o.dt_txt).format("DD") > currentDate) {
+            currentDate = moment(o.dt_txt).format("DD");
+            data.push(o);
+        }
+    })
+    return data;
 }
 
 // Create 5 Day forcast cards
-function createWeekForcast(date, temp, wind, humidity) {
-    let cDate = document.createElement("h5").classList.add("card-title").innerHTML(date);
-    let cTemp = document.createElement("p").classList.add("card-text").innerHTML(`Temp: ${temp} °F`);
-    let cWind = document.createElement("p").classList.add("card-text").innerHTML(`Wind: ${wind} MPH`);
-    let cHumidity = document.createElement("p").classList.add("card-text").innerHTML(`Humidity: ${humidity}%`);
+async function createWeekForcast() {
+    var data = await convertCity();
+    console.log(data);
+    for (var i = 0; i < data.length; i++) {
+        var cardRow = $('#forcast-cards');
+        var cardCol = $(`<div class="col"></div>`)
+        var cardH = $(`<div class="card h-100"></div>`)
+        var cardBody = $(`<div class="card-body"></div>`)
+        let cDate = $(`<h5 class="card-title">${formatDate(data[i].dt_txt)}</h5>`)
+        let cTemp = $(`<p class="card-text">Temp: ${data[i].main.temp} °F</p>`)
+        let cWind = $(`<p class="card-text">Wind: ${data[i].wind.speed} MPH</p>`)
+        let cHumidity = $(`<p class="card-text">Humidity: ${data[i].main.humidity}%</p>`)
 
-    cardRow.appendChild(cardCol);
-    cardCol.appendChild(cardH);
-    cardH.appendChild(cardBody)
-    cardBody.appendChild(cDate);
-    cardBody.appendChild(cIcon);
-    cardBody.appendChild(cTemp);
-    cardBody.appendChild(cWind);
-    cardBody.appendChild(cHumidity);
+
+        cardRow.append(cardCol);
+        cardCol.append(cardH);
+        cardH.append(cardBody)
+        cardBody.append(cDate);
+        // cardBody.appendChild(cIcon);
+        cardBody.append(cTemp);
+        cardBody.append(cWind);
+        cardBody.append(cHumidity);
+    }
 }
 
 // getForecastData();
 // convertCity();
-// createWeekForcast();
-convertCity();
+createWeekForcast();
+addCityHistory();
+// convertCity();
