1de3944f2a52df57ea5c4e18299f5cd5deec6116
Joseph Archer <viewtiful.kja@gmail.com>
2022-10-23 11:01:51 -0400

    finished the lat and lon conversion

--- script.js
+++ script.js
@@ -6,25 +6,60 @@ var city = 'Chicago';
 var queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKey}`
 var searchBtn = document.getElementById("search");
 
+// DOM Containers
+var cardRow = document.getElementById('forcast-cards');
+var cardCol = document.createElement('div').classList.add("col");
+var cardH = document.createElement('div').classList.add("card", "h-100");
+var cardBody = document.createElement('div').classList.add("card-body");
+
 // searchBtn.addEventListener("click",)
 
-function getForecastData() {
+async function getForecastData() {
     // Convert city name to lat and lon
-    fetch(queryURL)
+    await fetch(queryURL)
         .then((res) => res.json())
         .then((data) => console.log(data));
-
-    // fetch(queryURL)
-    //     .then((res) => res.json())
-    //     .then((data) => console.log(data));
-
+    return data
 
 }
 
-// function convertCity() {
-//     fetch(`https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=${APIKey}`)
-//     .then((res) => res.json())
-//     .then((data) => console.log(data));
-// }
+// Convert city to latitude and longitude
+async function convertCity() {
+    let lat = '';
+    let lon = '';
+    await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=${APIKey}`)
+        .then((res) => res.json())
+        .then((data) => data.forEach((c) => {
+            if (c.country === 'US') {
+                console.log(c.lat, c.lon);
+                lat = c.lat;
+                lon = c.lon;
+            }
 
-getForecastData();
+        }))
+    return fetch(`http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${APIKey}`)
+        .then((response) => response.json())
+        .then((weekData) => console.log(weekData.list));
+}
+
+// Create 5 Day forcast cards
+function createWeekForcast(date, temp, wind, humidity) {
+    let cDate = document.createElement("h5").classList.add("card-title").innerHTML(date);
+    let cTemp = document.createElement("p").classList.add("card-text").innerHTML(`Temp: ${temp} °F`);
+    let cWind = document.createElement("p").classList.add("card-text").innerHTML(`Wind: ${wind} MPH`);
+    let cHumidity = document.createElement("p").classList.add("card-text").innerHTML(`Humidity: ${humidity}%`);
+
+    cardRow.appendChild(cardCol);
+    cardCol.appendChild(cardH);
+    cardH.appendChild(cardBody)
+    cardBody.appendChild(cDate);
+    cardBody.appendChild(cIcon);
+    cardBody.appendChild(cTemp);
+    cardBody.appendChild(cWind);
+    cardBody.appendChild(cHumidity);
+}
+
+// getForecastData();
+// convertCity();
+// createWeekForcast();
+convertCity();
